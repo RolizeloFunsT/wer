c8399db2b524fd25b17dfe9742c7f5fcfdb66c9f
Glitch (hello-express) <none>
2020-01-19 19:32:45 +0000

    🏔🏃 Checkpoint ./komutlar/yardım2.js:5880028/101

--- komutlar/yardım2.js
+++ komutlar/yardım2.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, params, args) => {
   const menü2 = new Discord.RichEmbed()
   .setColor(0x36393e)
       .setAuthor('Kategoriler Yapımcı : By DumPS')
-     .addField(`Leroux Komut Menüsü`, `<a:emoji_6:654361680620421120>  **Komutlar Menü 2 **  <a:emoji_6:654361680620421120>  \n \n **istediginiz yazı kopyala yapıstır yapabilirsıni`)
+     .addField(`Leroux Komut Menüsü`, `<a:emoji_6:654361680620421120>  **Komutlar Menü 2 **  <a:emoji_6:654361680620421120>  \n \n **|istediginiz yazı kopyala yapıstır yapabilirsıniz.|** sonra ne komut u oldugunu yazın. sonra bunu yapın \n yine aynısı`)
     .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
   return message.channel.sendEmbed(menü2);
 
@@ -18,12 +18,12 @@ exports.run = async (client, message, params, args) => {
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['komut2', 'komutlar2', 'command2', 'yardım2', 'help2', 'halp2', 'y2', 'h2', 'commands2'],
+    aliases: ['komut', 'komutlar', 'command', 'yardım', 'help', 'halp', 'y', 'h', 'commands'],
     permLevel: 0
   };
   
   exports.help = {
-    name: 'yardım2',
-    description: 'yardım2',
-    usage: 'yardım2'
+    name: 'yardım',
+    description: 'yardım',
+    usage: 'yardım'
   };
